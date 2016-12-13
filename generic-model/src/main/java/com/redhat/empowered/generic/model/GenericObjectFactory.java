396eb2012a4bd89c076c1100c8184d74926c3f69
apham <apham@redhat.com>
2016-12-13 18:09:17 +0100

    clean up all pom files for consistency added h2DB

--- generic-model/src/main/java/com/redhat/empowered/generic/model/GenericObjectFactory.java
+++ generic-model/src/main/java/com/redhat/empowered/generic/model/GenericObjectFactory.java
@@ -1,5 +1,9 @@
 package com.redhat.empowered.generic.model;
 
+
+import org.springframework.beans.BeanUtils;
+
+
 public class GenericObjectFactory {
 
 	public StatisticsRecord createStatisticsRecord(String key) {
@@ -7,18 +11,10 @@ public class GenericObjectFactory {
 		statisticsRecord.setKey(key);
 		return statisticsRecord;
 	}
-	
+
 	public IndicatorRecord createIndicatorRecord(IndicatorRecord indicatorRecord){
-		IndicatorRecord newIndicatorRecord = new IndicatorRecord();
-		
-		// mapping data
-		newIndicatorRecord.setUid(indicatorRecord.getUid());
-		newIndicatorRecord.setValue(indicatorRecord.getValue());
-		newIndicatorRecord.setTimestmp(indicatorRecord.getTimestmp());
-		newIndicatorRecord.setFrequencyGroupValue(indicatorRecord.getFrequencyGroupValue());
-		newIndicatorRecord.setIndicatorClass(indicatorRecord.getIndicatorClass());
-		
-		return newIndicatorRecord;
-		
+		IndicatorRecord newIndicatorRecord  = new IndicatorRecord();
+		BeanUtils.copyProperties(indicatorRecord, newIndicatorRecord);
+		return newIndicatorRecord;		
 	}
 }
